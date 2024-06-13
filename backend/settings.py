58c397ad8f0bf2a9044e34d8622268c80ab34b2e
Erik Hromek <erikhromek96@gmail.com>
2024-06-13 12:33:57 -0300

    Change user model on settings

--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,9 @@ if CORS_ALLOWED_ORIGINS:
 else:
     CORS_ALLOWED_ORIGINS = []
 
+
+AUTH_USER_MODEL = "users.User"
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -203,8 +206,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "users.User"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 

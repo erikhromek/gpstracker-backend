b2a041fca7823cd2d305dc096e452ae7a53b8072
Erik Hromek <erikhromek96@gmail.com>
2024-06-13 12:59:16 -0300

    Fix asgi for production

--- backend/asgi.py
+++ backend/asgi.py
@@ -12,12 +12,13 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django_channels_jwt.middleware import JwtAuthMiddlewareStack
 
 from backend.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django_asgi_app = get_asgi_application()
+
+from django_channels_jwt.middleware import JwtAuthMiddlewareStack  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
